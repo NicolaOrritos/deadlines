fc6ab606e2e2076d2fbc3805ebdbe59d70a71c75
Nicola Orritos <nicola.orritos@gmail.com>
2014-01-06 23:56:40 +0100

    Initial works

---
+++ deadlines.js
@@ -0,0 +1,59 @@
+
+(function()
+{
+    function hideNewDeadline()
+    {
+        $(".deadline.new form").hide();
+        $(".deadline.new .save").hide();
+        $(".deadline.new .hide").hide();
+        $(".deadline.new .add").show();
+        
+        $(".deadline.new form [type=text]").val("");
+    }
+    
+    
+    $(document).ready(function()
+    {
+        $(".deadline.new form").hide();
+        $(".deadline.new .hide").hide();
+        $(".deadline.new .save").hide();
+        
+        $(".deadline.new .add").click(function(event)
+        {
+            $(".deadline.new form").show();
+            $(".deadline.new .save").show();
+            $(".deadline.new .hide").show();
+            $(".deadline.new .add").hide();
+        });
+        
+        $(".deadline.new .hide").click(function(event)
+        {
+            hideNewDeadline();
+        });
+        
+        $(".deadline.new .save").click(function(event)
+        {
+            var name = $(".deadline.new form [name=name]").val();
+            var date = $(".deadline.new form [name=date]").val();
+            
+            console.log("Creating new deadline named '%s' for date '%s'", name, date);
+            
+            // $.post("localhost:8124/deadlines/new", {"name": name, "date": date});
+            
+            
+            // If POST succeeds add to the UI a new deadline:
+            var newDeadline = "<div class=\"deadline\">"
+                            + "     <a class=\"del\" href=\"#\">‒</a>"
+                            + "     <form>"
+                            + "         <input name=\"name\" type=\"text\" placeholder=\"" + name + "\" />"
+                            + "         <input name=\"date\" type=\"text\" placeholder=\"" + date + "\" />"
+                            + "     </form>"
+                            + "     <a class=\"save\" href=\"#\">+</a>"
+                            + "</div>";
+            
+            $("#deadlines").prepend(newDeadline);
+            
+            hideNewDeadline();
+        });
+    });
+})();
